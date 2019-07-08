0238bbae340c64683e3e4ef498d553f5856c834b
DreamDarker <120844854@qq.com>
2019-07-08 16:01:44 +0800

    add RGB to single channel

---
+++ RGB2single.py
@@ -0,0 +1,36 @@
+# coding:utf-8
+
+import numpy as np
+import cv2
+import os
+from PIL import Image
+# 源目录
+MyPath = 'C:/Users/12084/Desktop/Proj/data/7-8 G/image/'
+# 输出目录
+OutPath = 'C:/Users/12084/Desktop/Proj/data/7-8 G/img/'
+
+def processImage(filesoure, destsoure, name):
+    '''
+    filesoure是存放待转换图片的目录
+    destsoure是存在输出转换后图片的目录
+    name是文件名
+    '''
+    # 打开图片
+    img = cv2.imread(filesoure + name)
+    # 提取G通道
+    b, g, r = cv2.split(img);
+    #创建与image相同大小的零矩阵
+    zeros = np.zeros(img.shape[:2],dtype="uint8")
+    cv2.imwrite(destsoure + name, cv2.merge([zeros, g, zeros]))  # [R, G, B]
+
+def run():
+    # 切换到源目录，遍历源目录下所有图片
+    os.chdir(MyPath)
+    for i in os.listdir(os.getcwd()):
+        #检查后缀
+        postfix = os.path.splitext(i)[1]
+        if  postfix == '.jpg':
+            processImage(MyPath, OutPath, i)
+
+if __name__ == '__main__':
+    run()
